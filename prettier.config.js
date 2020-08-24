2f77c8d361ce760eed479ef3a75a70a718087ac3
davidgortega <g.ortega.david@gmail.com>
2020-08-24 23:21:07 +0200

    restyler

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  semi: false,
+  arrowParens: 'avoid',
   singleQuote: true,
   trailingComma: 'none',
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   proseWrap: 'always'
-};
+}

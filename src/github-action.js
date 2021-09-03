d74cd4f8b153d8307e8c7c551c859821458d16c3
davidgortega <g.ortega.david@gmail.com>
2021-09-03 13:50:40 +0200

    lintfix

--- src/github-action.js
+++ src/github-action.js
@@ -4,7 +4,7 @@ const { setupCml } = require('./utils');
 (async () => {
   try {
     const version = core.getInput('version');
-    const sudo    = core.getBooleanInput('sudo'); 
+    const sudo = core.getBooleanInput('sudo');
     await setupCml({ version, sudo });
   } catch (error) {
     core.setFailed(error.message);

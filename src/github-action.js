5bbcfd96e8e47d2b8399bd036d145eb73e1541bc
DavidGOrtega <g.ortega.david@gmail.com>
2021-09-07 12:56:57 +0200

    Optional sudo usage (#36) (#38)
    * Optional sudo usage (#36)
    
    * update core/actions
    
    * test changes
    
    * update action.yml
    
    * update readme
    
    * add to readme
    
    * lintfix
    
    * fix typo
    
    * minor typo
    
    Co-authored-by: Daniel Barnes <dabarnes2b@gmail.com>
    Co-authored-by: Casper da Costa-Luis <casper.dcl@physics.org>

--- src/github-action.js
+++ src/github-action.js
@@ -4,7 +4,8 @@ const { setupCml } = require('./utils');
 (async () => {
   try {
     const version = core.getInput('version');
-    await setupCml({ version });
+    const sudo = core.getBooleanInput('sudo');
+    await setupCml({ version, sudo });
   } catch (error) {
     core.setFailed(error.message);
   }

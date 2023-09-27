483c972c4bc6cfb9ae07ebeb916c0317a276e470
Daniel Barnes <dabarnes2b@gmail.com>
2023-09-27 08:35:52 -0700

    setup

--- src/github-action.js
+++ src/github-action.js
@@ -1,12 +1,18 @@
 const core = require('@actions/core');
-const { setupCml } = require('./utils');
+const { setupCml, isNode16 } = require('./utils');
 
 (async () => {
   try {
     const version = core.getInput('version');
     const sudo = core.getBooleanInput('sudo');
     const force = core.getBooleanInput('force');
-    await setupCml({ version, sudo, force });
+
+    if (await isNode16()) {
+      await setupCml({ version, sudo, force });
+      core.info('Consider Migrating to setup-cml@v2: link');
+    } else {
+      core.warning('CML may not function properly see: link');
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
